234c792a0ade80bb8d97f9c3ad9826834ccab333
GEYSER_MULDONG <gsmuldong1@student.hau.edu.ph>
2024-10-19 12:51:05 +0800

    first commit

--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -20,7 +20,6 @@ from django.contrib import admin
 from django.urls import path, include  # Import include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home  # Import home view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
